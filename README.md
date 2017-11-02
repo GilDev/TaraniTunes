4c7ccc50b51d159c7ee29da78b293b4818713f37
GilDev <gildev@gmail.com>
2017-11-02 22:05:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+TaraniTunes
+===========
+
+*Awesome music player for FrSky Taranis radios.*
+
+Compatible with FrSky's [Taranis Q X7](https://www.frsky-rc.com/product/taranis-q-x7-2), [Taranis X9D](https://www.frsky-rc.com/product/taranis-x9d-plus-2) and their variants.
+You need at least [OpenTX](http://www.open-tx.org) 2.2 with Lua scripts enabled (check "lua" when downloading firmware in [OpenTX Companion](http://www.open-tx.org/2017/05/30/opentx-2.2.0)).
+
+* Taranis Q X7  
+  ![Taranis Q X7](Screenshots/TaraniTunesQX7.png)
+* Taranis X9D  
+  ![Taranis X9D](Screenshots/TaraniTunesX9D.png)
+
+
+Installation instructions
+-------------------------
+
+**I will try to add a video explaining TaraniTunes' installation and usage in the future**
+
+The “[Example](Example)” folder contains an exemple of the Taranis' SD card structure you must follow. It works, you can, if you want, merge it with your Taranis' current SD card content.
+
+1. On your computer:
+	1. Edit [`iTunes.lua`](iTunes.lua) according to your preferences if needed then put iTunes.lua in `/SCRIPT/TELEMETRY`.
+	2. Create a [`playlist.txt`](Example/SOUNDS/playlist.txt) file in `/SOUNDS` where each line must be formatted like this: `Song name:SONG_FILENAME` where `SONG_FILENAME` must be 6 characters or less. Don't end the file with a newline! Look at [Example/SOUNDS/playlist.txt](Example/SOUNDS/playlist.txt) for an example of formatting.
+	3. Put your corresponding songs `SONG_FILENAME.wav` in `/SOUNDS/en` if your radio is in English (otherwise replace `en` with your language, where you put your other sound files). They must be encoded in Microsoft WAV 16-bits signed PCM at a 32 kHz sampling rate, you can use [Audacity](http://www.audacityteam.org/) to do that, it works great.
+
+2. On your Taranis (I'm going to explain how I setup my radio)
+	1. Go to “DISPLAY” model's setting screen, set a screen to “Script” mode and choose “iTunes”.  
+	![Display settings](Screenshots/DisplaySettings.png)
+	2. Set “LOGICAL SWITCHES” model's setting screen as follow:  
+	![Logical switches settings](Screenshots/LogicalSwitchesSettings.png)
+	3. Set “FLIGHT MODES” model's setting screen as follow:  
+	![Flight modes settings](Screenshots/FlightModesSettings.png)  
+	In fact, put every throttle trims to “`--`” for every flight mode you use.
+
+There you go! Next section will explain you how to use TaraniTunes.
+
+Usage
+-----
+
+From the main menu, hold “Page” to access TaraniTunes. If everything has been setup correctly, your songs should appear, otherwise:
+
+* If you modified your [`iTunes.lua`](iTunes.lua), maybe some configuration values are wrong.
+* The `playlist.txt` syntax may be wrong. Be sure to put the song name, then the colon “:”, then the song filename of 6 characters maximum. Also make sure there is no empty line at the end, or you will get a “CPU Limit” error, yeah I probably need to fix that…
+
+1. Use the rotary encoder (Q X7) or the “+”/“-” buttons to sweep through songs.
+2. Press “Enter” to choose a song to play.
+3. Put the “SD” switch in the middle position to start playing. Put it back in the up position to stop.
+4. If you select another song, you have to stop and start playback (“SD” up then center again). I'm sorry I can't do it any other way…
+5. Put “SD” in the down position to select a random song from your playlist. Stop and start playback to play it.
+6. You can press throttle trims down and up to play next and previous song respectively. You still have to stop and start playback to play it.
+
+
+Todo
+----
+
+* Automatically play next music after one is finished (maybe skip to next song when already 30 seconds have been played?)
+
+
+Suggestions for OpenTX's API
+----------------------------
+
+* Constants to determine the radio type, useful when calculating switche's index because in special functions, logical switch 1 have the index 51 on Taranis X9D and 39 on Taranis Q X7.
+* A function called when entering a telemetry screen. Useful to draw the screen *only* when needed and not every frame. Without that variable, when entering the telemetry screen more than once, the screen stays stuck onto the main screen.
+* `table.insert(table, value)` doesn't seem to work, need to use `table[#table + 1] = value` instead.
---
+++ iTunes.lua
@@ -0,0 +1,199 @@
+-- TaraniTunes
+-- http://github.com/GilDev/TaraniTunes
+-- By GilDev
+-- http://gildev.tk
+
+-- CONFIG --
+
+local specialFunctionId = 30 -- This special function will be reserved: 1 for SF1, 2 for SF2…
+
+-- You NEED to use logical switches to manipulate TaraniTunes.
+
+-- WARNING: If you use trims for changing songs for example (and I recommand you do),
+-- you need to deactivate the "real" trim functions of the model, to do this,
+-- go into "FLIGHT MODES" configuration, go to each flight mode you use for your model
+-- and set the throttle "Trims" value (the third if you use the default AETR mode) to "--".
+
+-- Here's how to setup the play switch to SD centered:
+-- LOGICAL SWITCH  L61
+-- Func   OR
+-- V1     SD-
+-- V2     SD-
+-- Then set playSongLogicalSwitch to 61 below.
+
+-- Here's how to setup the next song switch to throttle trim down:
+-- LOGICAL SWITCH  L62
+-- Func   OR
+-- V1     tTd
+-- V2     tTd
+-- Then set nextSongLogicalSwitch to 62 below.
+
+local playSongLogicalSwitch   = 61 -- Logical switch that will play the current song
+local nextSongLogicalSwitch   = 62 -- Logical switch that will set the current song to the next one
+local prevSongLogicalSwitch   = 63 -- Logical switch that will set the current song to the previous one
+local randomSongLogicalSwitch = 64 -- Logical switch that will set the current song to a random one
+
+-- DON'T EDIT BELOW THIS LINE --
+
+local playlistFilename = "/SOUNDS/playlist.txt"
+local errorOccured = false
+
+local playingSong = 2
+local selection = 3
+
+local function error(strings)
+	errorStrings = strings
+	errorOccured = true
+end
+
+function playSong()
+	model.setCustomFunction(
+		specialFunctionId,
+		{
+			switch = playSongSwitchId,
+			func = 16,
+			name = playlist[playingSong][2]
+		}
+	)
+end
+
+local function init()
+	-- Calculate indexes
+	specialFunctionId  = specialFunctionId - 1 -- SF1 is at index 0 and so on
+	if LCD_W == 212 then -- if Taranis X9D
+		playSongSwitchId = 50 + playSongLogicalSwitch
+	else -- if Taranis Q X7
+		playSongSwitchId = 38 + playSongLogicalSwitch
+	end
+	nextSongSwitchId   = getFieldInfo("ls" .. nextSongLogicalSwitch).id
+	prevSongSwitchId   = getFieldInfo("ls" .. prevSongLogicalSwitch).id
+	randomSongSwitchId = getFieldInfo("ls" .. randomSongLogicalSwitch).id
+
+	-- Build playlist
+	playlist = {{"", ""}, {"", ""}}
+	playlistFile = io.open(playlistFilename, "r")
+
+	if (playlistFile == nil) then
+		error({"\"" .. playlistFilename .. "\"", "file not found, please", "read the \"README.md\"", "file for installation", "instructions."})
+		return
+	end
+	
+	repeat
+		songName = ""
+		songFilename = ""
+
+		-- Determine song name
+		char = ""
+		repeat
+			songName = songName .. char
+			char = io.read(playlistFile, 1)
+		until char == ":"
+
+		-- Determine song filename
+		char = ""
+		repeat
+			songFilename = songFilename .. char
+			char = io.read(playlistFile, 1)
+		until char == "\n" or char == ""
+
+		playlist[#playlist + 1] = {songName, songFilename} -- table.insert() doesn't work
+
+	until char == ""
+
+	io.close(playlistFile)
+
+	-- These blank starting and ending values are used when selecting top or bottom songs
+	playlist[#playlist + 1] = {"", ""}
+	playlist[#playlist + 1] = {"", ""}
+end
+
+nextSongSwitchPressed   = false;
+prevSongSwitchPressed   = false;
+randomSongSwitchPressed = false;
+local function background()
+	-- Next song
+	if getValue(nextSongSwitchId) > 0 then
+		if not nextSongSwitchPressed then
+			nextSongSwitchPressed = true
+			if playingSong < #playlist - 2 then
+				playingSong = playingSong + 1
+				playSong()
+			end
+		end
+	else
+		nextSongSwitchPressed = false
+	end
+
+	-- Previous song
+	if getValue(prevSongSwitchId) > 0 then
+		if not prevSongSwitchPressed then
+			prevSongSwitchPressed = true
+			if playingSong > 3 then
+				playingSong = playingSong - 1
+				playSong()
+			end
+		end
+	else
+		prevSongSwitchPressed = false
+	end
+
+	-- Random song
+	if getValue(randomSongSwitchId) > 0 then
+		if not randomSongSwitchPressed then
+			randomSongSwitchPressed = true
+			playingSong = getTime() % (#playlist - 4) + 3
+			playSong()
+		end
+	else
+		randomSongSwitchPressed = false
+	end
+end
+
+local function run(event)
+	-- INPUT HANDLING --
+	if (event == EVT_ROT_RIGHT or event == EVT_MINUS_FIRST or event == EVT_MINUS_RPT)
+		and selection < #playlist - 2 then
+		selection = selection + 1
+	elseif (event == EVT_ROT_LEFT or event == EVT_PLUS_FIRST or event == EVT_PLUS_RPT) and selection > 3 then
+		selection = selection - 1
+	elseif event == EVT_ROT_BREAK or event == EVT_ENTER_BREAK then -- Play selected song
+		playingSong = selection
+		playSong()
+	end
+
+	-- DRAWING --
+	lcd.clear();
+
+	-- Title
+	lcd.drawText(1, 1, "TaraniTunes", MIDSIZE)
+	lcd.drawText(LCD_W - 19, 1, "By", SMLSIZE)
+	lcd.drawText(LCD_W - 27, 9, "GilDev", SMLSIZE)
+
+	-- Separator
+	lcd.drawLine(0, 16, LCD_W - 1, 16, SOLID, FORCE)
+
+	-- Print error
+	if errorOccured then
+		yCoordinates = {18, 26, 34, 42, 50}
+		for i = 1, #errorStrings do
+			lcd.drawText(1, yCoordinates[i], errorStrings[i])
+		end
+
+		return
+	end
+
+	-- Now playing
+	lcd.drawText(1, 18, string.char(62) .. playlist[playingSong][1])
+
+	-- Separator
+	lcd.drawLine(0, 26, LCD_W - 1, 26, DOTTED, FORCE)
+
+	-- Song selector
+	lcd.drawText(1, 28, playlist[selection - 2][1], SMLSIZE)
+	lcd.drawText(3, 35, playlist[selection - 1][1], SMLSIZE)
+	lcd.drawText(1, 42, string.char(126) .. playlist[selection][1], SMLSIZE)
+	lcd.drawText(3, 49, playlist[selection + 1][1], SMLSIZE)
+	lcd.drawText(1, 56, playlist[selection + 2][1], SMLSIZE)
+end
+
+return {run = run, background = background, init = init}
