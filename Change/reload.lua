6e0554a8bd804c45f40bdd75ef852e071afb4e0c
John Wieland <john.r.wieland@gmail.com>
2018-04-06 14:43:38 -0400

    Add files via upload

---
+++ Change/reload.lua
@@ -0,0 +1,66 @@
+local fileToLoad="/SCRIPTS/TELEMETRY/choose.lua"
+local active = true
+
+local thisPage={}
+local page={}
+
+local function clearTable(t)
+  if type(t)=="table" then
+    for i,v in pairs(t) do
+      if type(v) == "table" then
+        clearTable(v)
+      end
+      t[i] = nil
+    end
+  end
+  collectgarbage()
+  return t 
+end
+
+thisPage.init=function(...)
+  if active then
+    page=dofile(fileToLoad)
+    page.init(...)
+  end
+  return true
+end
+
+thisPage.background=function(...)
+  if active then
+    page.background(...)
+  end
+  return true
+end
+
+thisPage.run=function(...)
+  local set=getValue("ls")
+  if set > 512 then 
+  set1=1 else
+  if set >= 0 then
+  set1=2 else
+  if set < 0 and set > -512 then
+  set1=3 else
+  set1=4
+  end
+  end
+  end   
+  
+  if active then
+    page.run(...)
+    active= not (...==EVT_ENTER_BREAK)
+  else
+    lcd.clear()
+    lcd.drawText( 34, 8, "!! New PlayList Request !!", BLINK)
+    lcd.drawText( 15, 20, "Select Playlist Slider Position", 0)
+    lcd.drawText( 50, 33, "Slider Position =" .. set1, 0)
+    lcd.drawText( 40, 45,"Press ENTER to Activate",0)
+    clearTable(page)
+    active= (...==EVT_ENTER_BREAK)
+    model.setTimer(2,{value=0})
+    thisPage.init()
+  end
+  return not (...==EVT_MENU_BREAK)
+    
+end
+
+return thisPage
