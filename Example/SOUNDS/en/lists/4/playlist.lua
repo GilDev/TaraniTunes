3c554fcb7d54bf08a18afc6e4ae82827ad921d2c
John Wieland <john.r.wieland@gmail.com>
2018-04-06 14:55:18 -0400

    Create playlist.lua

---
+++ Example/SOUNDS/en/lists/4/playlist.lua
@@ -0,0 +1,7 @@
+playlist = {
+	{"Alex List 4- Music 1", "Music1",25},
+	{"Dan List 4- Music 2", "Music2",15},
+	{"Fiona List 4- Music 3", "Music3",18},
+	{"Sam List 4- Music 4", "Music4",45},
+	{"Vic List 4- Music 5", "Music5",30},
+}
